4cd429ef82b070428e88dffccff9b3f4e2a4470c
Lucas Id Betan <lucas.idbetan@gmail.com>
2024-05-14 11:34:11 -0300

    added white bg to CompleteAudio

--- src/app/components/audio/completeAudio.tsx
+++ src/app/components/audio/completeAudio.tsx
@@ -4,7 +4,7 @@ import MediaControls from "./mediaButtons/mediaControls";
 
 export default function CompleteAudio({ audio }: { audio: string }) {
 	return (
-		<div className="flex flex-col justify-center items-center w-50% shadow-md  hover:translate-y-2 transition ease-in-out delay-150 hover:shadow-lg rounded-full py-2 px-4">
+		<div className="bg-white flex flex-col justify-center items-center w-50% shadow-md  hover:translate-y-2 transition ease-in-out delay-150 hover:shadow-lg rounded-full py-2 px-4">
 			<p className="font-bold">Audio Completo</p>
 			<MediaControls audio={audio} start={0} />
 		</div>
